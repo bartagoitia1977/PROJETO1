3965fdc6500d086c41612476ea5154e390846ef3
bartagoitia1977 <33225450+bartagoitia1977@users.noreply.github.com>
2018-09-29 21:48:19 -0300

    Add files via upload

--- Documento.py
+++ Documento.py
@@ -123,21 +123,24 @@ class Documento:
 	'''
 	CLASSE DOCUMENTO
 	'''
-	def __init__(self,documento):
-		self._documento = documento
+	def __init__(self,documento1,documento2):
+		self._documento1 = documento1
+		self._documento2 = documento2
 		
-	def contencao(self):
+	def palavras1(self):
 		'''
 		Concentra todas as palavras do documento adotando filtro de letras minusculas e minimizando
 		a poluição por caracteres alheios: espacos, pontos de interrogacao, exclamacao, etc.
+		Documento1
 		'''
-		self._lista_de_palavras = np.array([])
+		self._lista_de_palavras1 = np.array([])
 		self._nova_palavra = True
 		self._palavra = ""
-		for letra in self._documento:
+		for letra in self._documento1:
 			if ((letra != " ") and (letra != ".") and (letra != ",") and (letra != "\n") and (letra != ";") and
 			 (letra != "?") and (letra != '"') and (letra != "(") and (letra != ")") and (letra != "[") and
-			  (letra != "]") and (letra != "{") and (letra != "}") and (letra != '\'') and (letra != "!")):
+			  (letra != "]") and (letra != "{") and (letra != "}") and (letra != '\'') and (letra != "*") and
+			   (letra != "!") and (letra != "+") and (letra != "-") and (letra != ":") and (letra != "/")):
 				if (ord(letra) >= 65) and (ord(letra) <= 90):
 					self._palavra += chr(ord(letra) + 32)
 					self._nova_palavra = True
@@ -146,23 +149,104 @@ class Documento:
 					self._nova_palavra = True
 			else:
 				if (self._nova_palavra == True):
-					self._lista_de_palavras = np.append(self._lista_de_palavras,self._palavra)
+					self._lista_de_palavras1 = np.append(self._lista_de_palavras1,self._palavra)
 					self._palavra = ""
 					self._nova_palavra = False
 		if (self._nova_palavra == True):
-			self._lista_de_palavras = np.append(self._lista_de_palavras,self._palavra)
+			self._lista_de_palavras1 = np.append(self._lista_de_palavras1,self._palavra)
 			self._palavra = ""
 
-		return self._lista_de_palavras
+		return self._lista_de_palavras1
 
-	def gerarNGramas(self):
+	def palavras2(self):
+		'''
+		Concentra todas as palavras do documento adotando filtro de letras minusculas e minimizando
+		a poluição por caracteres alheios: espacos, pontos de interrogacao, exclamacao, etc.
+		Documento2
+		'''
+		self._lista_de_palavras2 = np.array([])
+		self._nova_palavra = True
+		self._palavra = ""
+		for letra in self._documento2:
+			if ((letra != " ") and (letra != ".") and (letra != ",") and (letra != "\n") and (letra != ";") and
+			 (letra != "?") and (letra != '"') and (letra != "(") and (letra != ")") and (letra != "[") and
+			  (letra != "]") and (letra != "{") and (letra != "}") and (letra != '\'') and (letra != "*") and
+			   (letra != "!") and (letra != "+") and (letra != "-") and (letra != ":") and (letra != "/")):
+				if (ord(letra) >= 65) and (ord(letra) <= 90):
+					self._palavra += chr(ord(letra) + 32)
+					self._nova_palavra = True
+				else:
+					self._palavra += letra
+					self._nova_palavra = True
+			else:
+				if (self._nova_palavra == True):
+					self._lista_de_palavras2 = np.append(self._lista_de_palavras2,self._palavra)
+					self._palavra = ""
+					self._nova_palavra = False
+		if (self._nova_palavra == True):
+			self._lista_de_palavras2 = np.append(self._lista_de_palavras2,self._palavra)
+			self._palavra = ""
+
+		return self._lista_de_palavras2
+
+	def gerarNGramas1(self):
 		'''
 		Gera lista encadeada contendo todos os Ngramas (5-grama) de um dado vetor de palavras.
+		Documento1
 		'''
-		self._lista_palavras = self.contencao()
-		self._lista_de_ngramas = LE()
+		self._lista_palavras1 = self.palavras1()
+		self._lista_de_ngramas1 = LE()
 		self._indice_ngrama = 0
-		for ng in range(len(self._lista_palavras) - 4):
-			self._lista_de_ngramas.anexar(Ngrama(self._lista_palavras,self._indice_ngrama))
+		for ng in range(len(self._lista_palavras1) - 4):
+			self._lista_de_ngramas1.anexar(Ngrama(self._lista_palavras1,self._indice_ngrama))
 			self._indice_ngrama += 1
-		return self._lista_de_ngramas
+		return self._lista_de_ngramas1
+
+	def gerarNGramas2(self):
+		'''
+		Gera lista encadeada contendo todos os Ngramas (5-grama) de um dado vetor de palavras.
+		Documento2
+		'''
+		self._lista_palavras2 = self.palavras2()
+		self._lista_de_ngramas2 = LE()
+		self._indice_ngrama = 0
+		for ng in range(len(self._lista_palavras2) - 4):
+			self._lista_de_ngramas2.anexar(Ngrama(self._lista_palavras2,self._indice_ngrama))
+			self._indice_ngrama += 1
+		return self._lista_de_ngramas2
+
+	def comparaNGramas(self,Ngrama_susp,Ngrama_ref):
+		self._Ngrama_susp = Ngrama_susp
+		self._Ngrama_ref = Ngrama_ref
+		self._teste_saida = False
+		self._count_string = 0
+		self._count_flag = 0
+		if (len(self._Ngrama_susp) != len(self._Ngrama_ref)):
+			self._teste_saida = False
+			return self._teste_saida
+		else:
+			for q in self._Ngrama_susp:
+				if (q != self._Ngrama_ref[self._count_string]):
+					self._count_flag += 1
+				self._count_string += 1
+			if (self._count_flag == 0):
+				self._teste_saida = True
+				return self._teste_saida
+			else:
+				self._teste_saida = False
+				return self._teste_saida
+
+	def contencao(self):
+		self._nlista1 = self.palavras1()
+		self._nlista2 = self.palavras2()
+		self._N1 = len(self._nlista1)
+		self._repeticoes = 0
+		for ng1 in self._nlista1:
+			self._flag = 0
+			for ng2 in self._nlista2:
+				if (self.comparaNGramas(ng1,ng2) == True):
+					if (self._flag == 0):
+						self._repeticoes += 1
+						self._flag = 1
+		self._contencao = (self._repeticoes / self._N1)
+		return self._contencao
